edfdd9035091577104670459c03aec99d712c583
loretari <loretari@gmail.com>
2024-04-11 09:13:17 +0300

    init: add Access-Control-Allow-Origin in index.js

--- server/index.js
+++ server/index.js
@@ -32,6 +32,15 @@ mongoose
         })
     })
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://incomparable-mermaid-e0ebfe.netlify.app'); // Pakeiskite į savo kliento aplikacijos URL
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
+
 app.use(cors({
     origin: "incomparable-mermaid-e0ebfe.netlify.app",
 
