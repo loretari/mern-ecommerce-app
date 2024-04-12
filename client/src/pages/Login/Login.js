95892f366ee6e804adbb50e2524d0a8cc0934370
loretari <loretari@gmail.com>
2024-04-12 16:03:09 +0300

    init: change Login

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -37,7 +37,7 @@ const Login = () => {
                 if (errorData && errorData.error === "Wrong password") {
                     setError(true);
                 } else {
-                    setError(false;)
+                    setError(false)
                 }
             }
 
